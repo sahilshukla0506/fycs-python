842f06e118e3eaed74fbdc677a9ebd6b659cb217
sahilshukla0506 <115152071+sahilshukla0506@users.noreply.github.com>
2025-07-24 15:59:47 +0530

    Add files via upload

---
+++ prac.py
@@ -0,0 +1,16 @@
+while True:
+     num = int(input(" Enter your num (type -1 to stop)"))
+     if num == -1:
+        print("loop ended with break")
+        break
+        
+     elif num == 0 :
+        print("zero is skipped")
+        continue
+        
+     elif num == 100:
+           pass
+        
+           print("100 was entered (pass executed)")
+
+           print("entered no :",num)
